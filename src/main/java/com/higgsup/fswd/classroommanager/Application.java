671ac2235d358216d44c34c4db508a212e5b5bad
tupm58 <kittkatt.liucuxiu@gmail.com>
2016-07-07 14:46:00 +0700

    merge ok

--- src/main/java/com/higgsup/fswd/classroommanager/Application.java
+++ src/main/java/com/higgsup/fswd/classroommanager/Application.java
@@ -16,7 +16,7 @@ import java.io.IOException;
 
 @Configuration
 class WebConfiguration {
-    //lll
+    //llljhhjhj
     @Bean
     ServletRegistrationBean h2servletRegistration() {
         ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
